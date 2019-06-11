23421d1d9896a2070d8b38b3bc2002c47333a9e4
Catherine Botelle <49196336+CathCB@users.noreply.github.com>
2019-06-11 10:56:08 -0300

    Add files via upload
    TÁ TUDO UMA BAGUNÇA :(

---
+++ P2.py
@@ -0,0 +1,84 @@
+
+'''Exercicio 1'''
+def media(nota1,nota2):
+    media = (nota1+nota2)/2
+    return media
+    
+'''Exercico 2'''
+
+linhas = []
+with open ('series.csv', 'r', enconding= 'utf-s') as file1:
+        linhas1 = series.csv.split()
+        
+with open ('series_novas.csv', 'r', enconding= 'utf-s') as file2:
+        linhas2 = series_novas.csv.split()
+        
+todas = open('todas.csv', 'w'):
+    linhas.append('linhas1')
+    linhas.append('linhas2')
+    print(linhas)
+
+'''Exercicio 3'''
+with open ('series_novas.csv', 'r', enconding= 'utf-s') as file2:
+        linhas = series_novas.csv.split()
+        print('Fuller House:{}episódios'.format(linhas.count('Fuller House'))
+        print('Designer Survivor:{}episódios'.format(linhas.count('Designer Survivor'))      
+        print('Suits:{}episódios'.format(linhas.count('Suits'))
+        print('Lucifer:{}episódios'.format(linhas.count('Lucifer'))
+        print('Narcos:{}episódios'.format(linhas.count('Narcos'))      
+              
+with open ('series.csv', 'r', enconding= 'utf-s') as file1:
+        linhas = series.csv.split()
+        print('The Big Bang Theory:{}episódios'.format(linhas.count('The Big Bang Theory'))
+        print('Friends:{}episódios'.format(linhas.count('Friend'))
+        print('Breaking Bad:{}episódios'.format(linhas.count('Breaking Bad'))
+        print('Black Mirror:{}episódios'.format(linhas.count('Black Mirror'))
+
+'''Exercicio 4'''
+linhas[6]=nota1
+linhas[7]=nota2
+
+with open ('series.csv', 'r', enconding= 'utf-s') as file:
+    for line in file.readlines():
+        linhas = line.split(',')
+              linhas[6]=nota1
+              linhas[7]=nota2
+              dic = {}
+              for i in range (0,605):
+              media(nota1, nota2)
+              dic1 = dic1.append{'linha[0]': 'media'}
+print(dic1)
+
+              
+'''Exercicio 5'''
+              
+linhas = []              
+with open ('series.csv', 'r', enconding= 'utf-s') as file1:
+    for line in file1.readlines():
+        linhas1 = line.split(',')
+              
+with open ('series.csv', 'r', enconding= 'utf-s') as file2:
+    for line in file2.readlines():
+        linhas2 = line.split(',')
+
+series_aprovadas = open('series_aprovadas.csv', 'w')
+        linhas.append('linhas1')
+        linhas.append('linhas2')
+              for i in range (0, 936):
+              if linhas[0] == 'Narcos' or 'Sense8' or 'The Big Bang Theory':
+                   linhas.remove([0], [1], [2], [3], [4], [5], [6])
+                   print(linhas)
+              elif:
+                   print(linhas)
+              
+                      
+
+              
+
+              
+              
+              
+              
+
+              
+              
